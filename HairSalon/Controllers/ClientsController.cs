0c94cd9c6efaab86044730f01899af605db8366d
Drake Wilcox <drakewilcox@gmail.com>
2020-03-20 11:20:54 -0700

    add routes and actions for Index and Create Views to controller.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -15,5 +15,23 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Restaurants.Include(Clients => Clients.Stylist).ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(clients);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
